02404833fb93c27dfcebb1e03b36781926dc9ca0
unknown <e.grigonis58@gmail.com>
2021-10-15 13:29:04 +0300

    FE refactor

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
-import Converter from './components/Converter';
-import LoginPage from './components/LoginPage';
-import RegisterPage from './components/RegisterPage';
+import Converter from './components/converter/Converter';
+import LoginPage from './components/pages/LoginPage';
+import RegisterPage from './components/pages/RegisterPage';
+import HomePage from './components/pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />
+      <RegisterPage />
     </div>
   );
 }
